554e855feac2478969bf60232d1ca61609fab2ef
songbiao <2845548112@qq.com>
2022-07-07 16:10:32 +0800

    新增对象映射器[将long数据类型转换为字符串类型，前端long类型会丢失精度]

--- src/main/java/com/biao/config/WebMvcConfig.java
+++ src/main/java/com/biao/config/WebMvcConfig.java
@@ -1,19 +1,40 @@
 package com.biao.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import com.biao.common.JacksonObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.util.List;
+
+@Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     /**
      * 设置静态资源映射
-     * */
+     */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展mvc框架的消息转换器");
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将java对象转换为json
+        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器容器中
+        converters.add(0, mappingJackson2HttpMessageConverter);
 
     }
 }
